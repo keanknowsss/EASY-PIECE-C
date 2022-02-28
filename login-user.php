55bda94c812cb889bc0e0fb7babb060c5dc1076a
GeloJacs <72681939+GeloJacs@users.noreply.github.com>
2022-02-28 22:02:33 +0800

    LOGIN
    binago name ng textbos sa email at bossword login

--- login-user.php
+++ login-user.php
@@ -6,12 +6,11 @@
         echo 'Failed to connect: ' . mysqli_connect_error();
     } else{
 
-        if($_SERVER['REQUEST_METHOD'] == "POST")
-        {
-            $Email = $_POST['email'];
+       
+            $Email = $_POST['emailCustomer'];
             $Password = $_POST['password'];
 
-            $query = "SELECT * FROM `registrationuser` WHERE email = '$Email' limit 1";
+            $query = "SELECT * FROM `registrationuser` WHERE Email = '$Email' limit 1";
 
             $result = mysqli_query($conn, $query);
 
@@ -27,7 +26,7 @@
                     }
                 }
             }
-         }
+         
     }
 
 ?>
