731ac6724f07d69c19df3fb22c92850ebf30c5dd
keanknowsss <solomon.keanu.sk@gmail.com>
2022-03-04 23:33:24 +0800

    Added automated navbar and converted files to php

--- regis-main.php
+++ regis-main.php
@@ -13,8 +13,8 @@
     <!-- BOOTSTRAP CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!-- STYLE CSS -->
-    <link rel="stylesheet" href="style.css">
+    <!-- JAVASCRIPT -->
+    <script type="text/JavaScript" src="script.js"></script>
 
     <!-- FONTAWESOME CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
